5c40e10934791eb3cb95e23f92c9f3d9ed378235
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-03 15:52:26 +0700

    Refactor load function to start function

--- bb/app.js
+++ bb/app.js
@@ -1,32 +1,27 @@
-async function load(){
-const cfg=await fetch("config.json");
-const json=await cfg.json();
-document.getElementById("title").innerHTML=json.title;
-document.getElementById("status").innerHTML=json.loadingText;
+async function start(){
 
-setTimeout(()=>{
-switch(json.activePlugin){
-case "whatsapp":
-location.href=
-"https://wa.me/"
-+json.plugins.whatsapp.phone+
-"?text="+
-encodeURIComponent(
-json.plugins.whatsapp.message
-);
-break;
+    const response=await fetch("config.json");
 
-case "website":
-location.href=
-json.plugins.website.url;
-break;
-case "maps":
-location.href=
-json.plugins.maps.url;
-break;
-}
-},800);
+    const config=await response.json();
+
+    document.title=config.title;
+
+    document.getElementById("title").innerHTML=config.title;
+
+    document.getElementById("status").innerHTML=config.loadingText;
+
+    const url=
+        "https://wa.me/"
+        +config.phone
+        +"?text="
+        +encodeURIComponent(config.message);
+
+    setTimeout(()=>{
+
+        window.location.href=url;
+
+    },config.delay);
 
 }
 
-load();
+start();
